e92a5ac6a862a59c915f269f146bbe542263b663
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-14 22:43:47 +0900

    handle action

--- TodoReducer.js
+++ TodoReducer.js
@@ -1,11 +1,19 @@
 import {combineReducers} from "redux";
 
 const InitialState = {
+    newTodo: '',
     list: [],
 };
 
 const todoReducer = (state = InitialState, action) => {
-    return state
+    switch (action.type) {
+        case 'ADD_TODO':
+            const {list} = state;
+            list.push(action.payload);
+            return {newTodo: "", list};
+        default:
+            return state;
+    }
 };
 
 export default combineReducers({todo: todoReducer})
