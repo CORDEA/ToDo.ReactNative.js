35aebd924c346f07543ee1046bd0d67449aefb5d
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-14 20:11:10 +0900

    update design

--- AddScreen.js
+++ AddScreen.js
@@ -1,5 +1,6 @@
-import {View} from "react-native";
+import {StyleSheet, TextInput, View} from "react-native";
 import React from "react";
+import {FloatingAction} from "react-native-floating-action";
 
 export default class AddScreen extends React.Component {
     static navigationOptions = {
@@ -8,7 +9,32 @@ export default class AddScreen extends React.Component {
 
     render() {
         return (
-            <View></View>
+            <View style={styles.container}>
+                <TextInput
+                    placeholder={"ToDo"}
+                />
+                <FloatingAction
+                    overrideWithAction
+                    actions={[
+                        {
+                            name: "add",
+                            icon: require("./img/baseline_add_white_24pt.png"),
+                        },
+                    ]}
+                    iconHeight={24}
+                    iconWidth={24}
+                    onPressItem={() => {
+                    }}
+                />
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#fff',
+    },
+});
