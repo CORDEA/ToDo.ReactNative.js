84750e1e4ff04516802ed193450509b38dbab849
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-14 16:49:21 +0900

    add reducer

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {createStore} from "redux";
+import todoReducer from './TodoReducer';
+import {Provider} from "react-redux";
+
+const store = createStore(todoReducer);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <Provider store={store}>
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+            </View>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
