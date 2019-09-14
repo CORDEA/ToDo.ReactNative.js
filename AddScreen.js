e531ac27b61f3522c7f704e8cc6aef89a0fd6dd5
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-14 20:01:43 +0900

    add screen

---
+++ AddScreen.js
@@ -0,0 +1,14 @@
+import {View} from "react-native";
+import React from "react";
+
+export default class AddScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Add ToDo',
+    };
+
+    render() {
+        return (
+            <View></View>
+        )
+    }
+}
